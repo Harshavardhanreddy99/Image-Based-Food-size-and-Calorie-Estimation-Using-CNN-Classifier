bfbe89c4e9690700d8df50288195224f8e65add4
prakyath12 <70650255+prakyath12@users.noreply.github.com>
2020-11-16 20:37:42 -0600

    Add files via upload

---
+++ Project Source Code/Development Phase 3/urls.py
@@ -0,0 +1,37 @@
+"""WebC URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+
+urlpatterns = [ 
+    path('', views.home, name="Welcome"),
+    path('adminlogin/', views.adminlogin, name="adminlogin"),  
+    path('adminhome/', views.adminhome, name="adminhome"),  
+    path('adminlogout/', views.adminlogout, name="adminlogout"),
+    path('userhome/', views.userhome, name="userhome"),  
+    path('userlogout/', views.userlogout, name="userlogout"),
+    path('uploaddataset/', views.uploaddataset, name="uploaddataset"), 
+    path('xlupload/', views.xlupload, name="xlupload"), 
+    path('viewdataset/', views.viewdataset, name="viewdataset"), 
+    path('signup/', views.signupaction, name="signup"), 
+    path('userlogin/', views.userlogin, name="userlogin"), 
+    path('getcalories/', views.getcalories, name="getcalories"), 
+    path('getcal/', views.getcal, name="getcal"), 
+    
+    
+]
---
+++ Project Source Code/Development Phase 3/views.py
@@ -0,0 +1,174 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse, request
+
+from django.template import Context
+
+import xlrd
+
+
+from .models import dataset
+from .models import users
+
+from .FOODCNN import CNN
+
+
+
+def home(request):
+	return render(request, 'index.html')
+
+def adminlogin(request):
+	if request.method=='POST':
+		uid=request.POST['uid']
+		pwd=request.POST['pwd']
+		
+		if uid=='admin' and pwd=='admin':
+			request.session['adminid']='admin'
+			return render(request, 'admin_home.html')
+
+		else:
+			return render(request, 'admin.html',{'msg':"Login Fail"})
+
+	else:
+		return render(request, 'admin.html')
+
+def adminhome(request):
+	if "adminid" in request.session:
+		uid=request.session["adminid"]
+		return render(request, 'admin_home.html')
+
+	else:
+		return render(request, 'admin.html')
+
+def adminlogout(request):
+	try:
+		del request.session['adminid']
+	except:
+		pass
+	return render(request, 'admin.html')	
+	
+def uploaddataset(request):
+	if "adminid" in request.session:
+		
+		return render(request, 'upload.html')
+
+	else:
+		return render(request, 'admin.html')
+
+def xlupload(request):
+	if "adminid" in request.session:
+		file=request.POST['file']
+		#file="E:\\"+file
+		book = xlrd.open_workbook(file)
+		sheet = book.sheet_by_index(0)
+		dataset.objects.all().delete()
+		for r in range(1, sheet.nrows):
+			f0 = sheet.cell(r, 0).value
+			f1 = sheet.cell(r, 1).value
+			d=dataset(name=f0, calorie=f1)
+			d.save()
+		return render(request, 'upload.html',{'msg':"Dataset Uploaded Successfully"})
+	else:
+		return render(request, 'admin.html')
+
+def viewdataset(request):
+	if "adminid" in request.session:
+		data=dataset.objects.all()
+		return render(request, 'viewdataset.html',{'data':data})
+		
+	else:
+		return render(request, 'admin.html')
+
+def signupaction(request):
+	if request.method=='POST':
+		email=request.POST['mail']
+		pwd=request.POST['pwd']
+		
+		tele=request.POST['tele']
+		name=request.POST['name']
+		
+		city=request.POST['city']
+
+		
+		d=users.objects.filter(email__exact=email).count()
+		if d>0:
+			return render(request, 'signup.html',{'msg':"Email Already Registered"})
+		else:
+			d=users(name=name,email=email,tele=tele,city=city,pwd=pwd)
+			d.save()
+			return render(request, 'signup.html',{'msg':"Register Success, You can Login.."})
+	else:
+
+		return render(request, 'signup.html')
+
+def userlogin(request):
+	if request.method=='POST':
+		uid=request.POST['mail']
+		pwd=request.POST['pwd']
+		d=users.objects.filter(email__exact=uid).filter(pwd__exact=pwd).count()
+		
+		if d>0:
+			d=users.objects.filter(email__exact=uid)
+			request.session['email']=uid
+			return render(request, 'user_home.html',{'data': d[0]})
+
+		else:
+			return render(request, 'userlogin.html',{'msg':"Login Fail"})
+
+	else:
+
+		return render(request, 'userlogin.html')
+
+
+
+def userhome(request):
+	if "email" in request.session:
+		uid=request.session["email"]
+		d=users.objects.filter(email__exact=uid)
+		return render(request, 'user_home.html',{'data': d[0]})
+
+	else:
+		return render(request, 'userlogin.html')
+
+def userlogout(request):
+	try:
+		del request.session['email']
+	except:
+		pass
+	return render(request, 'userlogin.html')	
+	
+def getcalories(request):
+	if "email" in request.session:
+		
+		return render(request, 'uploadimage.html')
+
+	else:
+		return render(request, 'userlogin.html')
+def getcal(request):
+	if request.method=='POST':
+		file=request.POST['file']
+		gram=int(request.POST['gram'])
+		print(file, gram)
+		
+		tagetimage='C:\\Users\\14694\\Desktop\\django\\FoodCalorie\\Testdata\\'+file
+		from .FOODCNN import CNN
+
+		name=CNN.detect(tagetimage)
+
+		d=dataset.objects.filter(name__exact=name)
+		cal=0.0
+		for d1 in d:
+			print(d1.calorie)
+			cal=float(d1.calorie)
+		cal=cal*gram
+		cal=int(cal)
+
+		d={'name':name,'cal':cal}
+		print(d)
+
+
+		return render(request, 'result.html',{'data': d})
+		
+	else:
+
+		return render(request, 'userlogin.html')
